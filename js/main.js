58d56649d9a5b433c389c79a0b746569df6c208e
Werner Ádám <adamprofi20@gmail.com>
2019-06-23 13:36:04 +0200

    Table default data

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@
 
 /* Dávid end */
 /* Ádam */
- 
+var tabel_manager = document.createElement('script');
+tabel_manager.src = "js/table_manager.js";
+document.head.appendChild(tabel_manager);
 /* Ádám end */
