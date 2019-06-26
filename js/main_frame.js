b8c53fe29aa82577cff058d050c2a3296aa53fa6
Werner Ádám <adamprofi20@gmail.com>
2019-06-26 02:09:05 +0200

    Submenu stucture is ready.

--- js/main_frame.js
+++ js/main_frame.js
@@ -9,7 +9,7 @@
 /**
  * Remove all active items from menu
  */
-function menuItemsClear() {    
+function menuItemsClear() {
     for (let index = 0; index < document.getElementsByClassName("menu-item").length; index++) {
         document.getElementsByClassName("menu-item")[index].classList.remove("menu-item-active");
     }
@@ -24,6 +24,26 @@ function menuItemsClear() {
 function menuItemClick(id) {
     menuItemsClear();
     document.getElementById(id).classList.add("menu-item-active");
+
+    switch (id) {
+        case "tab_finance":
+
+            break;
+        case "tab_processes":
+
+            break;
+        case "tab_products":
+
+            break;
+        case "tab_resources":
+
+            break;
+        case "tab_settings":
+
+            break;
+        default:
+            break;
+    }
 }
 /** Click events end */
 /** Events end */
