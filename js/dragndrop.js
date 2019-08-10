87330180bc4a4642c3ef3e87e445e518ddfda613
Áron Kósa <kosa.aron98@gmail.com>
2019-08-10 11:14:29 +0200

    drag and drop with hidden employee buttons

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -1,38 +1,44 @@
-$(document).ready(function(){
-    alert("jquery is ready");
-});
+
 $(".slides").sortable({
-     placeholder: 'slide-placeholder',
+    placeholder: 'slide-placeholder',
     axis: "y",
     revert: 150,
-    start: function(e, ui){
-        
-        placeholderHeight = ui.item.outerHeight();
+    start: function (e, ui) {
+
+        placeholderHeight = 40;
         ui.placeholder.height(placeholderHeight + 15);
         $('<div class="slide-placeholder-animator" data-height="' + placeholderHeight + '"></div>').insertAfter(ui.placeholder);
-    
+        var appBanners = document.getElementsByClassName('employee-button'), i;
+
+        for (var i = 0; i < appBanners.length; i++) {
+            appBanners[i].style.display = 'none';
+        }
     },
-    change: function(event, ui) {
-        
+    change: function (event, ui) {
+
         ui.placeholder.stop().height(0).animate({
-            height: ui.item.outerHeight() + 15
+            height: 60
         }, 300);
-        
-        placeholderAnimatorHeight = parseInt($(".slide-placeholder-animator").attr("data-height"));
-        
+
+        placeholderAnimatorHeight = 40;
+
         $(".slide-placeholder-animator").stop().height(placeholderAnimatorHeight + 15).animate({
             height: 0
-        }, 300, function() {
+        }, 300, function () {
             $(this).remove();
             placeholderHeight = ui.item.outerHeight();
             $('<div class="slide-placeholder-animator" data-height="' + placeholderHeight + '"></div>').insertAfter(ui.placeholder);
         });
-        
+
     },
-    stop: function(e, ui) {
-        
+    stop: function (e, ui) {
+
         $(".slide-placeholder-animator").remove();
-        
+        var appBanners = document.getElementsByClassName('employee-button'), i;
+
+        for (var i = 0; i < appBanners.length; i++) {
+            appBanners[i].style.display = 'inline-block';
+        }
+
     },
 });
-    
