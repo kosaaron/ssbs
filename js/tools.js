e408f4faac745569c9511c40036bee0d481f219a
Áron Kósa <kosa.aron98@gmail.com>
2019-11-06 10:45:44 +0100

    Adding week-reservation element to tool cards

--- js/tools.js
+++ js/tools.js
@@ -23,10 +23,10 @@ let Cards = {
     getToolsCard : function() {
         let container = "";
         container += '<div class="col-lg-12"><div id="tool_card_*5*" class="card toolcard tool-show-details"><div class="card-body">';
-        container += '!<div class="display-flex justify-content-between"><div class="tool-image-container"><img class="tool-image" src="https://images.obi.hu/product/HU/800x600/292962_1.jpg"><img class="tool-image" src="../images/gifs/toolweek.GIF">';
-        container += '!<button type="button" class="btn btn-primary tool-tag *4*"!>*4*</button></div>';
+        container += '!<div class="display-flex justify-content-between"><div class="tool-image-container"><img class="tool-image" src="https://images.obi.hu/product/HU/800x600/292962_1.jpg"></div>';
         container += '!<div class="tool-datas"><h3 class="card-title tool-name">*2*</h3>';
         container += '!<p class="tool-detail"><i class="fas fa-map-pin"></i> Helye: *3*</p>';
+        container += '<div class="d-flex justify-content-between week-container"> <div class="week-day week-day-red"><p class="day-name">H</p></div><div class="week-day week-day-green"><p class="day-name">K</p></div><div class="week-day week-day-red"><p class="day-name">Sz</p></div><div class="week-day week-day-green"><p class="day-name">Cs</p></div><div class="week-day week-day-green"><p class="day-name">P</p></div></div>'
         container += '</div></div></div></div></div>';
     
         return container;
