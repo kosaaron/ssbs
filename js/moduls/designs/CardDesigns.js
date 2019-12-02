962fb6dfd8d90083b7a92f7592cfb5d4dd4321b1
Áron Kósa <kosa.aron98@gmail.com>
2019-12-02 12:43:52 +0100

    HTML outsourcing in tools

--- js/moduls/designs/CardDesigns.js
+++ js/moduls/designs/CardDesigns.js
@@ -5,10 +5,48 @@ export default class CardDesigns {
     /** CC cards */
     getSimpleCard(shellId) {
         return `
-            <div class="col-lg-6"><div id="${shellId}_card_*1*" class="card taskcard ${shellId}-show-details"><div class="card-body">
-            !<h5 class="text-o-ellipsis card-title">*2*</h5>
-            !<p class="card-text">*3*</p>
-            </div></div></div>
+        <div class="col-lg-6">
+            <div id="${shellId}_card_*1*" class="card taskcard ${shellId}-show-details">
+                <div class="card-body">
+                    !<h5 class="text-o-ellipsis card-title">*2*</h5>
+                    !<p class="card-text">*3*</p>!
+                </div>
+            </div>
+        </div>
+        `
+    }
+    getToolCard(shellId) {
+        return `
+        <div class="col-lg-12">
+            <div id="${shellId}_card_*5*" class="card toolcard ${shellId}-show-details">
+                <div class="card-body">
+                    <div class="display-flex justify-content-between">
+                        <div class="tool-image-container"><img class="tool-image" src="https://images.obi.hu/product/HU/800x600/292962_1.jpg"></div>
+                        <div class="tool-datas">
+                            !<h3 class="card-title tool-name">*2*</h3>
+                            !<p class="tool-detail"><i class="fas fa-map-pin"></i> Helye: *3*</p>
+                            <div class="d-flex justify-content-between week-container">
+                                <div class="week-day week-day-red">
+                                    <p class="day-name">H</p>
+                                </div>
+                                <div class="week-day week-day-green">
+                                    <p class="day-name">K</p>
+                                </div>
+                                <div class="week-day week-day-red">
+                                    <p class="day-name">Sz</p>
+                                </div>
+                                <div class="week-day week-day-green">
+                                    <p class="day-name">Cs</p>
+                                </div>
+                                <div class="week-day week-day-green">
+                                    <p class="day-name">P</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         `
     }
 }
