7b13d6d1dca3626daeb88fa3c292858d4cd06cef
Áron Kósa <kosa.aron98@gmail.com>
2020-01-25 11:38:56 +0100

    Login system with ajax

--- js/login.js
+++ js/login.js
@@ -8,7 +8,8 @@
 /** Imports */
 
 function loadLogin() {
-    document.getElementById("btn_help").addEventListener("click", openHelp, false)
+    document.getElementById("btn_help").addEventListener("click", openHelp, false);
+    document.getElementById("btn_login").addEventListener("click", checkLogin, false);
 }
 window.onload = loadLogin;
 
@@ -21,4 +22,32 @@ function openHelp(){
         x.style.display = "none";
         document.getElementById("btn_help").innerHTML='<i class="fas fa-question-circle"></i>';
     }
+}
+function checkLogin(){
+    var email = document.getElementById("login_input_email").value;
+    var password = document.getElementById("login_input_password").value;
+
+    //Connect to Filter.php
+    $.ajax({
+        type: "POST",
+        url: "./php/Authenticate.php",
+        data: { 
+            Email: email,
+            Password: password
+        },
+        success: function (data) {
+            var x = document.getElementById('login_message_container')
+            if(data['LoggedIn']){
+                x.innerHTML='<p>' + data['Message'] + '</p><i class="fas fa-check-circle"></i>';
+                x.style.display= "block";
+                window.location.replace("index.html");
+            }
+            else{
+                x.style.display= "block";
+                x.innerHTML='<p>' + data['Message'] + '</p><i class="fas fa-times-circle"></i>';
+            }
+            
+        },
+        dataType: 'json'
+    });    
 }
