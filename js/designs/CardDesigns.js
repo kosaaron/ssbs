122675e61d09866d678fba3eafdfc4c98c89527f
Áron Kósa <kosa.aron98@gmail.com>
2020-01-30 10:29:37 +0100

    Deleting rate from employee card

--- js/designs/CardDesigns.js
+++ js/designs/CardDesigns.js
@@ -58,7 +58,6 @@ export default class CardDesigns {
                         <div class="employee-image-container display-flex align-items-center"><i class="far fa-user"></i></div>
                         <div class="employee-datas">
                             !<h3 class="card-title employee-name">*4* !*5*</h3>
-                            !<span class="employee-rate"><i class="far fa-star"></i> *6*</span>
                             !<p class="employee-position">*2*</p>
                             !<p class="employee-detail">Összes költség: <span>*3*</span> forint/hó</p>
                         </div>
