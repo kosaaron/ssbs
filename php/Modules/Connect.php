8c2c06c96b7f74597ddf5203c5db021868c020e1
Werner Ádám <adamprofi20@gmail.com>
2020-03-26 23:10:14 +0100

    can switch from localhost to remote access

--- php/Modules/Connect.php
+++ php/Modules/Connect.php
@@ -4,12 +4,19 @@ class PDOConnect
     public $pdo;
     function __construct()
     {
+        
         $servername = "ssbsystem.com";
         $username = "ssbsyste_server";
         $password = "Sport2018";
+        $database = "ssbsyste_ssbs";
+/*
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "ssbs";*/
 
         try {
-            $pdo = new PDO("mysql:host=$servername;dbname=ssbsyste_ssbs;charset=utf8", $username, $password);
+            $pdo = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
             // set the PDO error mode to exception
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo = $pdo;
