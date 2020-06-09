ae7a7402f4d973ed7c51274144026c1e23c30eba
Werner Ádám <adamprofi20@gmail.com>
2020-06-09 12:51:56 +0200

    Test sql

--- sql/test.sql
+++ sql/test.sql
@@ -6,24 +6,12 @@ FROM tasks
 WHERE tasks.Name LIKE '%%' && tasks.TaskTypeFK LIKE '%%'
 LIMIT 20;
 SELECT tasks.TaskId AS '1',
-    tasks.Name AS '2'
-FROM tasks
-WHERE tasks.Name LIKE '%%' && tasks.TaskTypeFK LIKE '%%'
-LIMIT 1;
-SELECT tasks.TaskId AS '1',
     orders.Name AS '2',
     orders.AliasId AS '3'
 FROM tasks
     INNER JOIN orders ON TaskFK = TaskId
 WHERE tasks.Name LIKE '%%' && tasks.TaskTypeFK LIKE '%%'
 LIMIT 20;
-SELECT tasks.TaskId AS '1',
-    task_types.Name AS '2',
-    tasks.Deadline AS '3'
-FROM tasks
-    INNER JOIN task_types ON TaskTypeFK = TaskTypeId
-WHERE tasks.Name LIKE '%%' && tasks.TaskTypeFK LIKE '%%'
-LIMIT 1;
 SELECT tasks.TaskId AS '1',
     orders.Name AS '2',
     orders.AliasId AS '3'
